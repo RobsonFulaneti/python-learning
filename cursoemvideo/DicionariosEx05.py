31b11b84e5e703f3f7e7859093c15587605c8afb
Robson Fulaneti <robsonfulaneti@gmail.com>
2020-03-05 18:11:17 -0300

    Corrigindo sintaxe

--- cursoemvideo/DicionariosEx05.py
+++ cursoemvideo/DicionariosEx05.py
@@ -1,29 +1,43 @@
-pessoa = dict()
 mulher = list()
+geral = list()
 acimaMedia = list()
 mediaIdade = 0
 idade = 0
 numeroPessoa = 0
 while True:
+
+    pessoa = dict()
     pessoa['nome'] = str(input("Digite o nome da pessoa: "))
     numeroPessoa += 1
+
     pessoa['sexo'] = str(input("Digite o sexo (M ou F): "))
 
-    if pessoa['sexo'] in 'Ff':
-        mulher.append(pessoa["nome"])
     pessoa['idade'] = int(input("Digite a idade: "))
     idade = pessoa['idade'] + idade
-    mediaIdade = idade/numeroPessoa
+
+    geral.append(pessoa.copy())
+
     opcao = str(input("Deseja inserir outra pessoa ? S/N "))
     if opcao in 'Nn':
         break
+
+mediaIdade = idade / numeroPessoa
+
+for pessoa in geral:
+
     if pessoa['idade'] > mediaIdade:
-        acimaMedia.append(pessoa.copy())
+        acimaMedia.append(pessoa['nome'])
+    if pessoa['sexo'] in 'Ff':
+        mulher.append(pessoa["nome"])
 print('=*' * 30)
-print(f'O número de pessoas cadastradas foram de {numeroPessoa} pessoas')
+print(f'\nAs mulheres cadastradas foram: ', end='')
+for k in mulher:
+    print(f'{k} ', end='')
+
+print(f'\nAs pessoas acima da média de idade são: ', end='')
+for k in acimaMedia:
+    print(f'{k} ', end='')
+print(f'\nO número de pessoas cadastradas foram de {numeroPessoa} pessoas')
 print('=*' * 30)
 print(f"A média de idade é de {mediaIdade:.1f} anos")
 print('=*' * 30)
-print(f'As mulheres cadastradas são {mulher}')
-print('=*' * 30)
-print(f'As pessoas com idade acima da média são: {acimaMedia}')
