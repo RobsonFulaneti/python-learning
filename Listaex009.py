237de06b008e3f8d8e69bcbdf202f2c947d3cae4
Robson Fulaneti <robsonfulaneti@gmail.com>
2020-03-07 18:08:11 -0300

    Corrigindo o texto

--- Listaex009.py
+++ Listaex009.py
@@ -2,14 +2,14 @@ from random import randint
 mega = list()
 jogos = list()
 tot = 0
-while tot <= 5:
+while tot <= 6:
     cont = 0
     while True:
         num = randint(1, 60)
         if num not in mega:
             mega.append(num)
             cont += 1
-        if cont >= 6:
+        if cont >= 7:
             break
     mega.sort()
     jogos.append(mega[:])
