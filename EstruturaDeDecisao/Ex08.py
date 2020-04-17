7caa716c6475c3988a698d1beea4b0385d1c5db5
Robson Fulaneti <robsonfulaneti@gmail.com>
2020-04-17 12:57:51 -0300

    Funções

--- EstruturaDeDecisao/Ex08.py
+++ EstruturaDeDecisao/Ex08.py
@@ -3,6 +3,29 @@ passaram = []
 recuperacao = []
 reprovados = []
 opcao = 0
+
+
+def mostraLinha():
+    print("=" * 68)
+
+
+def valorMedia(valor1, valor2):
+    calculo = (valor1 + valor2) / 2
+    return calculo
+
+
+def processaMedia(media):
+    if media >= 7:
+        print(f"{aluno} passou com media {media}!")
+        passaram.append([aluno, media])
+    elif media >= 3:
+        print(f"{aluno} está de recuperação pois fechou com media {media}")
+        recuperacao.append([aluno, media])
+    else:
+        print(f"{aluno} não passou pois fechou com media {media}")
+        reprovados.append([aluno, media])
+
+
 while opcao != 6:
     print("="*30, " MENU ", "="*30)
     print("[1] Para adicionar alunos\n"
@@ -11,46 +34,39 @@ while opcao != 6:
           "[4] Para listar quem nao passou\n"
           "[5] Para alunos cadastrados\n"
           "[6] Para sair")
-    print("=" * 68)
+    mostraLinha()
     opcao = int(input("Digite a opção: "))
     if opcao == 1:
         while True:
-            print("=" * 68)
+            mostraLinha()
             aluno = str(input("Digite o nome do aluno: "))
             alunos.append(aluno)
             nota1 = float(input("Digite a primeira nota: "))
             nota2 = float(input("Digite a segunda nota: "))
-            media = (nota1 + nota2) / 2
-            if media >= 7:
-                print(f"{aluno} passou com media {media}!")
-                passaram.append([aluno, media])
-            elif media >= 3:
-                print(f"{aluno} está de recuperação pois fechou com media {media}")
-                recuperacao.append([aluno, media])
-            else:
-                print(f"{aluno} não passou pois fechou com media {media}")
-                reprovados.append([aluno, media])
-            print("=" * 68)
+            media = valorMedia(nota1, nota2)
+            processaMedia(media)
+
+            mostraLinha()
             sair = str(input("Voltar ao menu principal ? S/N "))
             if sair in 'Ss':
                 break
     if opcao == 2:
-        print("=" * 68)
+        mostraLinha()
         print("Os alunos aprovados foram:", end=''"\n")
         for i, j in passaram:
             print(i, "com nota",j)
     if opcao == 3:
-        print("=" * 68)
+        mostraLinha()
         print("Os alunos de recuperacao foram:", end=''"\n")
         for i, j in recuperacao:
             print(i, "com nota",j)
     if opcao == 4:
-        print("=" * 68)
+        mostraLinha()
         print("Os alunos reprovados foram:", end=''"\n")
         for i, j in reprovados:
             print(i, "com nota",j)
     if opcao == 5:
-        print("=" * 68)
+        mostraLinha()
         print("Os alunos cadastrados foram:", end=''"\n")
         for i in alunos:
             print(i)
@@ -59,5 +75,3 @@ while opcao != 6:
     if opcao not in range(1, 7):
         print("Opção inválida, digite novamente")
 print("Obrigado por utilizar o programa")
-
-print("Ola mundo")
