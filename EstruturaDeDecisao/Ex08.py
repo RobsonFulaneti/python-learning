2ddbf7f4915286404eba2a70d087a84bb4c27449
Robson Fulaneti <robsonfulaneti@gmail.com>
2020-04-20 17:47:26 -0300

    inserindo funções

--- EstruturaDeDecisao/Ex08.py
+++ EstruturaDeDecisao/Ex08.py
@@ -9,6 +9,16 @@ def mostraLinha():
     print("=" * 68)
 
 
+def mostraMenu():
+    print("="*30, " MENU ", "="*30)
+    print("[1] Para adicionar alunos\n"
+          "[2] Para listar quem passou\n"
+          "[3] Para listar quem está de recuperação\n"
+          "[4] Para listar quem nao passou\n"
+          "[5] Para alunos cadastrados\n"
+          "[6] Para sair")
+
+
 def valorMedia(valor1, valor2):
     calculo = (valor1 + valor2) / 2
     return calculo
@@ -27,22 +37,19 @@ def processaMedia(media):
 
 
 while opcao != 6:
-    print("="*30, " MENU ", "="*30)
-    print("[1] Para adicionar alunos\n"
-          "[2] Para listar quem passou\n"
-          "[3] Para listar quem está de recuperação\n"
-          "[4] Para listar quem nao passou\n"
-          "[5] Para alunos cadastrados\n"
-          "[6] Para sair")
+    mostraMenu()
     mostraLinha()
+
     opcao = int(input("Digite a opção: "))
     if opcao == 1:
         while True:
             mostraLinha()
+
             aluno = str(input("Digite o nome do aluno: "))
             alunos.append(aluno)
             nota1 = float(input("Digite a primeira nota: "))
             nota2 = float(input("Digite a segunda nota: "))
+
             media = valorMedia(nota1, nota2)
             processaMedia(media)
 
